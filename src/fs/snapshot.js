018a030a45c0f97c972315e0d516f07d2f4d3513
Jahongir Hayitov <Jahongirhacking@gmail.com>
2026-03-03 16:01:52 +0500

    feat: complete snapshot.js

--- src/fs/snapshot.js
+++ src/fs/snapshot.js
@@ -1,9 +1,51 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import process from "node:process";
+
 const snapshot = async () => {
-  // Write your code here
-  // Recursively scan workspace directory
-  // Write snapshot.json with:
-  // - rootPath: absolute path to workspace
-  // - entries: flat array of relative paths and metadata
+  const root = process.cwd();
+  const files = await fs.readdir(process.cwd(), {
+    recursive: true,
+    withFileTypes: true,
+  });
+  const entries = await Promise.all(
+    files?.map(async (file) => {
+      const fullPath = path.join(file?.parentPath, file?.name);
+      const fileStat = await fs.stat(fullPath);
+      let fileContent = "";
+      try {
+        if (file?.isFile()) {
+          fileContent = await fs.readFile(fullPath);
+        }
+      } catch (err) {
+        console.error("Cannot read file:", fullPath, err.message);
+      }
+
+      return {
+        path: path.relative(root, fullPath),
+        type: file?.isFile() ? "file" : "directory",
+        ...(file?.isFile()
+          ? {
+              size: fileStat.size,
+              content: fileContent ? fileContent.toString("base64") : "",
+            }
+          : {}),
+      };
+    }),
+  );
+  fs.writeFile(
+    "./snapshot.json",
+    JSON.stringify(
+      {
+        rootPath: root,
+        entries,
+      },
+      null,
+      2,
+    ),
+    "utf-8",
+  );
+  console.log("snapshot.json is created successfully!");
 };
 
 await snapshot();
